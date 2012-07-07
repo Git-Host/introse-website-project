2f517d6f5a45fcb9500481930adae274776e1467
roxannesapitan <roxannesapitan@gmail.com>
2012-07-07 20:57:33 +0800

    SMS ABOUT OFFICERS PAGE. UPDATE 1

---
+++ SMS_aboutoff.php
@@ -0,0 +1,179 @@
+<html>
+<head>
+<link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+<title>Samahan ng mga Mag-aaral ng Sikolohiya</title>
+<link rel="stylesheet" type="text/css" href="master.css" />
+</head>
+
+<body>
+
+<div class='whole'>
+<br><br><br>
+<p align=center class="font">
+<br><br>
+<a href="SMS_aboutus.php">About the Organization</a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="SMS_aboutoff.php"><font color="red"><b>About the Officers</b></font></a>
+<br><br>
+<a class="thumbnail">
+<img src="officers/1.png" />
+<span><img src="officers/1.png" alt="President" width="100" height="150" >Name<br>Cellphone<br>Email Add</span></a>
+<br>PRESIDENT<br>
+<img src="officers/oneline.png" />
+<br>
+<img src="officers/2.png" />
+<br>EVP<br>
+<img src="officers/line.png" />
+
+	<div class="VPs" >
+		<div class="VPfinance">
+			<img src="officers/3.png" />
+		</div>
+		
+		<div class="VPdocu">
+			<img src="officers/4.png" />
+		</div>
+		
+		<div class="VPact">
+			<img src="officers/5.png" />
+		</div>
+		
+		<div class="VPmark">
+			<img src="officers/6.png" />
+		</div>
+		<div class="VPall">
+			<img src="officers/7.png" />
+		</div>
+		<br><br><br><br><br>
+		<font style="font-family: 'Play', sans-serif;">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		VP FINANCE
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		VP DOCUMENTATION
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		VP ACTIVITIES
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		VP MARKETING
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		VP ALLIANCES
+		</font>
+	</div>
+
+<br><br><br><br><br>
+<p align="center">
+<img src="officers/line2.png" />
+	<div class="AVPs" >
+		<div class="AVPfinance1">
+			<img src="officers/8.png" />
+		</div>
+		
+		<div class="AVPfinance2">
+			<img src="officers/9.png" />
+		</div>
+		
+		<div class="AVPdocu1">
+			<img src="officers/10.png" />
+		</div>
+		
+		<div class="AVPdocu2">
+			<img src="officers/11.png" />
+		</div>
+		
+		<div class="AVPact1">
+			<img src="officers/12.png" />
+		</div>
+		
+		<div class="AVPact2">
+			<img src="officers/13.png" />
+		</div>
+		
+		<div class="AVPact3">
+			<img src="officers/14.png" />
+		</div>
+		
+		<div class="AVPmark1">
+			<img src="officers/15.png" />
+		</div>
+		
+		<div class="AVPmark2">
+			<img src="officers/16.png" />
+		</div>
+		
+		<div class="AVPmark3">
+			<img src="officers/17.png" />
+		</div>
+		
+		<div class="AVPall1">
+			<img src="officers/18.png" />
+		</div>
+		
+		<div class="AVPall2">
+			<img src="officers/19.png" />
+		</div>
+		<br><br><br><br><br>
+		<font style="font-family: 'Play', sans-serif;">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP FINANCE
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP PRE-ACTS
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP AB-ACADEMICS
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP MEMBERSHIP
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP INTERNAL ALLIANCES
+		</font>
+		<br><br><br><br><br><br><br>
+		<font style="font-family: 'Play', sans-serif;">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP FINANCE
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP POST-ACTS
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP BS-ACADEMICS
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP PUBLICITY
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP EXTERNAL ALLIANCES
+		</font>
+		<div class="AVPlabel">
+		<font style="font-family: 'Play', sans-serif;">
+		AVP NON-ACADEMICS
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		AVP PUBLICATION
+		</font>
+		</div>
+	</div>
+</p>
+</p>
+</div>
+
+
+<div class='banner'>
+
+	<div class='cdiv'>
+	<br>
+	<p align=center>
+		<a href="index.php"><img src="logo.png"/></a>
+	</p>
+	</div>
+	
+	<div class='rdiv'><br>
+	<br><br><br><br><br>
+	<p align=center>
+		<a href="SMS_aboutus.php"><img src="about us.png"/></a>
+	</p>
+	</div>
+	
+	<div class='ldiv'>
+	<br><br><br><br><br><br>
+	<p align=center>
+		<a href="SMS_calendar.php"><img src="calendar.png"/></a>
+	</p>
+	</div>
+
+</div>
+
+</body>
+
+</html>
