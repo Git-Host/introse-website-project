ec4cff38b6779a4dee947e8c0aa2e334cb689a17
roxannesapitan <roxannesapitan@gmail.com>
2012-07-07 20:56:49 +0800

    SMS ABOUT US PAGE. UPDATE 1

---
+++ SMS_aboutus.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Marck+Script' rel='stylesheet' type='text/css'>
+<title>Samahan ng mga Mag-aaral ng Sikolohiya</title>
+<link rel="stylesheet" type="text/css" href="master.css" />
+</head>
+
+<body>
+
+<div class='whole'>
+<br><br><br>
+<p align=center class="font">
+<br><br>
+<a href="SMS_aboutus.php"><font color="red"><b>About the Organization</b></font></a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="SMS_aboutoff.php">About the Officers</a>
+<br><br><br><br>
+<div class="misvis">
+<p align=center class="fontcursive">Samahan ng mga Mag-aaral sa Sikolohiya, the official Psychology organization of De La Salle University- Manila (DLSU-M), seeks to unify the Psychology community by catering to their interests and needs. It promotes Sikolohiyang Pilipino and aims to further develop Psychology as a discipline in DLSU-M by providing talks, seminars, symposia, socio-civic events and activities to students.
+</p>
+</div>
+</p>
+</div>
+
+
+<div class='banner'>
+
+	<div class='cdiv'>
+	<br>
+	<p align=center>
+		<a href="SMS_home.php"><img src="logo.png"/></a>
+	</p>
+	</div>
+	
+	<div class='rdiv'><br>
+	<br><br><br><br><br>
+	<p align=center>
+		<a href="SMS_aboutus.php"><img src="about us.png"/></a>
+	</p>
+	</div>
+	
+	<div class='ldiv'>
+	<br><br><br><br><br><br>
+	<p align=center>
+		<a href="SMS_calendar.php"><img src="calendar.png"/></a>
+	</p>
+	</div>
+
+</div>
+
+</body>
+
+</html>
