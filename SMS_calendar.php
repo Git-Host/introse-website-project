0eac54fc363be109603b4537681915030e0de308
roxannesapitan <roxannesapitan@gmail.com>
2012-07-07 20:55:08 +0800

    SMS CALENDAR PAGE. UPDATE 1

---
+++ SMS_calendar.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>Samahan ng mga Mag-aaral ng Sikolohiya</title>
+<link rel="stylesheet" type="text/css" href="master.css" />
+</head>
+
+<body>
+
+<div class='whole'>
+<br><br><br>
+<p align=center>
+<iframe src="https://www.google.com/calendar/embed?src=dlsu.sms%40gmail.com&ctz=Asia/Manila" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe></p>
+</div>
+
+
+<div class='banner'>
+
+	<div class='cdiv'>
+	<br>
+	<p align=center>
+		<a href="SMS_home.php"><img src="logo.png"/></a>
+	</p>
+	</div>
+	
+	<div class='rdiv'><br>
+	<br><br><br><br><br>
+	<p align=center>
+		<a href="SMS_aboutus.php"><img src="about us.png"/></a>
+	</p>
+	</div>
+	
+	<div class='ldiv'>
+	<br><br><br><br><br><br>
+	<p align=center>
+		<a href="SMS_calendar.php"><img src="calendar.png"/></a>
+	</p>
+	</div>
+
+</div>
+
+</body>
+
+</html>
